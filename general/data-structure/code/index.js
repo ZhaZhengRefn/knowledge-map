d2a63321e6b529faa1765d5dfca09694aeb20168
zhazheng <512557403@qq.com>
2018-08-31 16:51:55 +0800

    实现linked-list

--- general/data-structure/code/index.js
+++ general/data-structure/code/index.js
@@ -1,10 +1,14 @@
-import LinkedList from './linked-list'
+const LinkedList = require('./linked-list')
 
 const linkedList = new LinkedList()
 
 linkedList.addAtHead(1)
 linkedList.addAtHead(2)
-linkedList.addAtHead(1)
+linkedList.addAtHead(3)
+linkedList.addAtHead(4)
+linkedList.addAtHead(5)
+// linkedList.addAtTail(2)
+const delResult = linkedList.deleteAtIndex(4)
 
-const result = linkedList.toArray()
-console.log(result)
+const result = linkedList.toString()
+console.log(result, delResult)
